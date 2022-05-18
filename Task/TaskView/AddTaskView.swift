0ab7fa82c1dc85fe46e3ecef3a9fcdaf6f0418b8
Wesley Jin <jw95097@hotmail.com>
2022-05-19 00:02:36 +1000

    Add and positioned task colour options (Red, Green, Blue), start on background colour change when user selects a colour

--- Task/TaskView/AddTaskView.swift
+++ Task/TaskView/AddTaskView.swift
@@ -11,7 +11,7 @@ struct AddTaskView: View {
     @EnvironmentObject var taskModel: TaskViewModel
     @Environment(\.self) var env
     var body: some View {
-        VStack(spacing: 10){
+        VStack(spacing: 20){
             Text("Add Task")
                 .font(.title2.bold())
                 .frame(maxWidth: .infinity)
@@ -24,14 +24,32 @@ struct AddTaskView: View {
                             .font(.title2)
                             .foregroundColor(.black)
                     }
-                    VStack(alignment: .leading, spacing:10) {
-                        Text("Choose Task Colour")
-                            .font(.title2)
-                            .foregroundColor(.black)
+                }
+                
+            VStack(alignment: .leading, spacing:20) {
+                Text("Choose Task Colour")
+                .font(.title2)
+                .foregroundColor(.black)
                         
-                        let colors: [String] = ["Red", "Blue", "Green"]
+            let colours: [String] = ["Red", "Green", "Blue"]
+                        
+            HStack(spacing: 40) {
+                ForEach(colours, id: \.self){
+                color in Circle()
+                    .fill(Color(color))
+                    .frame(width: 30, height: 30)
+                    /*.background{
+                        if taskModel.taskColor == color {
+                            Circle()
+                                .strokeBorder(.black)
+                                .padding(-10)
+                        }
+                    }*/
                     }
                 }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 20)
         }
         .frame(maxHeight: .infinity, alignment: .top)
         .padding()
